ce92f7cfd284dff936439275a03a5f3f501a449d
Alex Mayer <aspiesteam@mail.ru>
2025-04-29 11:25:26 +0300

    Made game field via JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import './App.css'
 
-
+const SYMBOL_X = 'X'
+const SYMBOL_O = 'O'
 
 function App() {
+  const currentStep = SYMBOL_O
+
+  const cells = [null, null, null, SYMBOL_O, SYMBOL_X, SYMBOL_O, null, null, null]
+
+  const getSymbolClassName = (symbol) => {
+    if (symbol === SYMBOL_X) return 'symbol--x'
+    else if (symbol === SYMBOL_O) return 'symbol--o'
+    else return ''
+  }
+
   return (
     <div className='game'>
       <div className='game-info'>
-        Ход: <span className='symbol symbol--x'>X</span>
+        Ход: <span className={ `symbol ${ getSymbolClassName(currentStep) }` }>{ currentStep }</span>
       </div>
       <div className="game-field">
-        <button className='cell cell--win'><span className="symbol symbol--x">X</span></button>
-        <button className="cell"><span className="symbol symbol--o">O</span></button>
-        <button className="cell"></button>
-        <button className="cell cell--win"><span className="symbol symbol--x">X</span></button>
-        <button className="cell"></button>
-        <button className="cell"><span className="symbol symbol--o">O</span></button>
-        <button className="cell cell--win"><span className="symbol symbol--x">X</span></button>
-        <button className="cell"><span className="symbol symbol--o">O</span></button>
-        <button className="cell"></button>
+        {cells.map(symbol => {
+          return (
+            <>
+              <button className='cell cell--win'>{
+                symbol && <span className={ `symbol ${ getSymbolClassName(symbol) }` }>{ symbol }</span>
+              }</button>
+            </>
+          ) 
+        })}
       </div>
     </div>
   )
